3d99dc4ba428fa2514fdd0f1bbe38ee92ccfc11e
Eric Henke <eahenke@gmail.com>
2017-09-04 11:01:00 -0500

    remove commented lines

--- test/test-utils.js
+++ test/test-utils.js
@@ -3,7 +3,6 @@ const supertest = require('supertest');
 function request(app, verb, uri)
 {
     return supertest(app)[verb](uri).set('Content-Type', 'application/json').set('Accept', 'application/json');
-    // .expect('Content-Type', /json/);
 }
 
 module.exports = {
